ccfcb618a4b3757699a3769803ff254965e1a1d0
liberbell <liberbell@gmail.com>
2018-10-11 08:49:58 +0900

    view3

--- myprj/myapp1/views.py
+++ myprj/myapp1/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(response):
-    return HttpResponse('<h1>Hello, myapp1!</h1>')
+def index(request):
+    # return HttpResponse('<h1>Hello, myapp1!</h1>')
+    return render(request, 'myapp1/index.html')
