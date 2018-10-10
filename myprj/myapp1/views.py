cff9d9fa9314c4f53c545d17d44ffa6ab894e3c4
liberbell <liberbell@gmail.com>
2018-10-11 08:57:39 +0900

    view4

--- myprj/myapp1/views.py
+++ myprj/myapp1/views.py
@@ -5,7 +5,7 @@ from django.template import loader
 # Create your views here.
 def index(request):
     # return HttpResponse('<h1>Hello, myapp1!</h1>')
-    template = loader.get_template('myapp/index.html')
+    template = loader.get_template('myapp1/index.html')
     context = {'fname':'hide'}
     return HttpResponse(template.render( context, request))
     # return render(request, 'myapp1/index.html')
