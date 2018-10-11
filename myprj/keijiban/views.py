6bdd7f49744af4313f73b70d8d00f055d9b2289d
liberbell <liberbell@gmail.com>
2018-10-12 07:49:26 +0900

    view2

--- myprj/keijiban/views.py
+++ myprj/keijiban/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from keijiban.form import KakikomiForm
 
 # Create your views here.
+def kakikomi(request):
+    f = KakikomiForm()
+    return HttpResponse(f)
