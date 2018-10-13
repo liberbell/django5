aa6b95453f010167509127677c36669ddba707df
liberbell <liberbell@gmail.com>
2018-10-13 13:10:23 +0900

    view1

--- myprj/keijiban/views.py
+++ myprj/keijiban/views.py
@@ -4,6 +4,10 @@ from keijiban.forms import KakikomiForm
 
 # Create your views here.
 def kakikomi(request):
-    f = KakikomiForm()
+    f = KakikomiForm({
+        'name' : ' hide'
+        'email' : 'hide@example.com'
+        'body' : 'Hello this is django form'}
+        )
     # return HttpResponse(f.as_table())
     return render(request, 'keijiban/kakikomiform.html', {'form1': f} )
