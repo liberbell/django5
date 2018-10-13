308a3c3239deccf08adda35531ebc0c9fe455448
liberbell <liberbell@gmail.com>
2018-10-13 13:34:47 +0900

    forms1

--- myprj/keijiban/forms.py
+++ myprj/keijiban/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class KakikomiForm(forms.Form):
      name = forms.CharField()
      email = forms.EmailField()
-     body = forms.CharField()
+     body = forms.CharField(widget=forms.Textarea)
