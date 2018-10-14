8210bb56b0d54a2c46d8a6d37b2a920bed84edc7
liberbell <liberbell@gmail.com>
2018-10-14 20:10:52 +0900

    view1

--- myprj/keijiban/views.py
+++ myprj/keijiban/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from keijiban.forms import KakikomiForm
+from myapp1.forms import LoginForm
 
 # Create your views here.
 def kakikomi(request):
@@ -20,3 +21,17 @@ def kakikomi(request):
         'keijiban/kakikomiform.html',
         {'form1' : f}
     )
+
+def index(request):
+    return render(request, 'myapp1/index.html')
+
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            if username == 'user1' and password == 'secret':
+                return HttpResponseRedirect('/myapp1/')
