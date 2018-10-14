dd681048846b6d0ac739f9ca98397bd71394e205
liberbell <liberbell@gmail.com>
2018-10-14 20:22:27 +0900

    settings4

--- myprj/myprj/settings.py
+++ myprj/myprj/settings.py
@@ -37,10 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django-widget-tweaks'
+    # 'django-widget-tweaks'
     'myapp1',
     'keijiban',
-    # 'widget-tweaks',
+    'widget-tweaks',
 ]
 
 MIDDLEWARE = [
